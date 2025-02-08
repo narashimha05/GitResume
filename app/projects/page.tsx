a9f136052de8feb445bd78d0dd7dade60d39d53d
narashimha05 <chinnarinarashimha@gmail.com>
2025-02-09 03:13:55 +0530

    finally ig

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -103,7 +103,7 @@ function ProjectsPageContent() {
   return (
     <div className="p-6">
             <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
-      <h1 className="text-3xl font-bold mb-4 text-center">Select Up to <span className="bg-green-300 px-2 rounded-lg">4 Projects</span></h1>
+      <h1 className="text-xl sm:text-3xl font-bold mb-4 text-center">Select Up to <span className="bg-green-300 px-2 rounded-lg">4 Projects</span></h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
         {projects.map((project) => (
           <button
