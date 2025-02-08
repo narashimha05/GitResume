63e9499511c5a6be953dd12d1cfae1e547be3608
narashimha05 <chinnarinarashimha@gmail.com>
2025-02-08 18:17:44 +0530

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 // import GitHubCalendar from 'react-github-calendar';
+"use client";
 import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className='border-black border-4 m-4 w-full'>
+    <div className='border-black border-4 w-full'>
       Hello
       {/* <div className='border-black border-4 m-4 w-full'>
         <GitHubCalendar username="narashimha05" colorScheme='light' />
       </div> */}
-      <button onClick={()=>router.push("/form")}>Forms</button>
+      <button onClick={()=>router.push("/form")} className="border-2 rounded-lg px-4 py-1 bg-gray-200 ml-4">Forms</button>
     </div>
   );
 }
